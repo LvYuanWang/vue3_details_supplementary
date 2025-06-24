902103914df201003205fbfcba74530311e98b23
LvYuanWang <1175430934@qq.com>
2025-06-24 20:57:14 +0800

    自定义指令 v_permission

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <h1>细节补充模板</h1>
-    <TestComponent />
-  </div>
+  <!-- 只有在权限为 read 的时候, 才显示这个按钮 -->
+  <button v-permission="['read']">读取</button>
+  <!-- 只有在权限为 write 的时候, 才显示这个按钮 -->
+  <button v-permission="['write']">写入</button>
+  <!-- 只有在权限为 manage 的时候, 才显示这个按钮 -->
+  <button v-permission="['manage']">管理</button>
 </template>
 
-<script setup>
-import TestComponent from './components/TestComponent.vue'
-</script>
+<script setup></script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/global.css'
 
 const app = createApp(App)
 
-app.config.globalProperties.Test = {
-  msg: 'Hello from Test',
-}
+// 权限数组
+const permissionArr = ['read', 'manage']
+
+// 全局权限控制指令
+app.directive('permission', {
+  mounted(el, { value }) {
+    if (value && value instanceof Array) {
+      const isPermission = value.some((item) => permissionArr.includes(item))
+      isPermission ? (el.style.display = 'block') : (el.style.display = 'none')
+    } else {
+      throw new Error('v-permission 的值必须是数组')
+    }
+  },
+})
 
 app.mount('#app')
